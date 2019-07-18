fc987e720b38ee06df5d6dee52816571dd60d6d7
Systraningo <nitingour99@gmail.com>
2019-07-18 15:04:00 +0530

    second commit

--- CJProject/src/FHDemo.java
+++ CJProject/src/FHDemo.java
@@ -3,7 +3,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
+//file handling
 public class FHDemo {
 	public static void main(String[] args) {
 //try with resource
